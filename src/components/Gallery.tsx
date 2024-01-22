0f237c7a4a665a6f862b10a4f29a4fc20ff6896c
Edgar Nightingale <edgar.nightingale@geniussports.com>
2024-01-22 14:48:29 +0000

    Add image hover

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { josefin } from "@/app/fonts";
 import { ImageType } from "@/utils/enums";
 import { getImageData } from "@/utils/helpers";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
@@ -15,8 +16,11 @@ export default function Gallery({ type }: IProps) {
 		<ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
 			<Masonry gutter="10px">
 				{images.map((image, index) => (
-					<div key={index}>
+					<div key={index} className="relative">
 						<img src={`/gallery${image.src}`} alt={image.name} loading="lazy" />
+						<div className={`${josefin.className} image-backdrop`}>
+							<p>{image.name}</p>
+						</div>
 					</div>
 				))}
 			</Masonry>
