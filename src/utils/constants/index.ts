4e19160e41eda73562f77cb551e01dfbaf04d851
Edgar Nightingale <edgar.nightingale@geniussports.com>
2024-01-22 12:36:58 +0000

    Compress all images

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -5,369 +5,369 @@ export const images: Record<ImageType, IImage[]> = {
 	[ImageType.Ceramics]: [
 		{
 			name: "Chequered Pseudosphere",
-			src: "/ceramics/Chequered-Pseudosphere.jpg",
+			src: "/ceramics/Chequered-Pseudosphere-min.jpg",
 		},
 		{
 			name: "DSCF0458",
-			src: "/ceramics/DSCF0458.JPG",
+			src: "/ceramics/DSCF0458-min.JPG",
 		},
 		{
 			name: "DSCF0460",
-			src: "/ceramics/DSCF0460.JPG",
+			src: "/ceramics/DSCF0460-min.JPG",
 		},
 		{
 			name: "DSCF0463",
-			src: "/ceramics/DSCF0463.JPG",
+			src: "/ceramics/DSCF0463-min.JPG",
 		},
 		{
 			name: "DSCF0465",
-			src: "/ceramics/DSCF0465.JPG",
+			src: "/ceramics/DSCF0465-min.JPG",
 		},
 		{
 			name: "DSCF0466",
-			src: "/ceramics/DSCF0466.JPG",
+			src: "/ceramics/DSCF0466-min.JPG",
 		},
 		{
 			name: "DSCF0469",
-			src: "/ceramics/DSCF0469.JPG",
+			src: "/ceramics/DSCF0469-min.JPG",
 		},
 		{
 			name: "DSCF0474",
-			src: "/ceramics/DSCF0474.JPG",
+			src: "/ceramics/DSCF0474-min.JPG",
 		},
 		{
 			name: "DSCF0478",
-			src: "/ceramics/DSCF0478.JPG",
+			src: "/ceramics/DSCF0478-min.JPG",
 		},
 		{
 			name: "DSCF0482",
-			src: "/ceramics/DSCF0482.JPG",
+			src: "/ceramics/DSCF0482-min.JPG",
 		},
 		{
 			name: "Fathers Day",
-			src: "/ceramics/Fathers-Day.jpg",
+			src: "/ceramics/Fathers-Day-min.jpg",
 		},
 		{
 			name: "Growth and Decay",
-			src: "/ceramics/growth-and-decay.jpg",
+			src: "/ceramics/growth-and-decay-min.jpg",
 		},
 		{
 			name: "Herissa Pot and SW Indian",
-			src: "/ceramics/herissa-pot-and-sw-indian.jpg",
+			src: "/ceramics/herissa-pot-and-sw-indian-min.jpg",
 		},
 		{
 			name: "Incense burner in action scarva",
-			src: "/ceramics/Incense-Burner-in-action-scarva.jpg",
+			src: "/ceramics/Incense-Burner-in-action-scarva-min.jpg",
 		},
 		{
 			name: "Nature and Nurture",
-			src: "/ceramics/Nature-and-Nurture-s.jpg",
+			src: "/ceramics/Nature-and-Nurture-s-min.jpg",
 		},
 		{
 			name: "Picture 002",
-			src: "/ceramics/Picture-002.jpg",
+			src: "/ceramics/Picture-002-min.jpg",
 		},
 		{
 			name: "Scarva Pots",
-			src: "/ceramics/Scarva-pots-300-dpi.jpg",
+			src: "/ceramics/Scarva-pots-300-dpi-min.jpg",
 		},
 	],
 	[ImageType.Oil]: [
 		{
 			name: "The Long Long Road",
-			src: "/oil/3-The-Long-Long-Road-Web.jpg",
+			src: "/oil/3-The-Long-Long-Road-Web-min.jpg",
 		},
 		{
 			name: "The Last of the few",
-			src: "/oil/5-The-Last-of-the-Few-Web.jpg",
+			src: "/oil/5-The-Last-of-the-Few-Web-min.jpg",
 		},
 		{
 			name: "Youth",
-			src: "/oil/7-Youth-Web.jpg",
+			src: "/oil/7-Youth-Web-min.jpg",
 		},
 		{
 			name: "The End of the Route",
-			src: "/oil/8-The-End-of-the-Route-Web.jpg",
+			src: "/oil/8-The-End-of-the-Route-Web-min.jpg",
 		},
 		{
 			name: "Nightmare on the Bridge",
-			src: "/oil/10-Nightmare-on-the-Bridge-Web.jpg",
+			src: "/oil/10-Nightmare-on-the-Bridge-Web-min.jpg",
 		},
 		{
 			name: "What Happened to Father",
-			src: "/oil/11-What-Happened-to-Father-Web.jpg",
+			src: "/oil/11-What-Happened-to-Father-Web-min.jpg",
 		},
 		{
 			name: "Father Returns to Earth",
-			src: "/oil/12-Father-Returns-to-Earth-Web.jpg",
+			src: "/oil/12-Father-Returns-to-Earth-Web-min.jpg",
 		},
 		{
 			name: "The Back Door",
-			src: "/oil/13-The-Back-Door-Web.jpg",
+			src: "/oil/13-The-Back-Door-Web-min.jpg",
 		},
 		{
 			name: "Breakpoint",
-			src: "/oil/15-Breakpoint-Webl.jpg",
+			src: "/oil/15-Breakpoint-Webl-min.jpg",
 		},
 		{
 			name: "Ships of the Desert",
-			src: "/oil/16-Ships-of-the-Desert-Web.jpg",
+			src: "/oil/16-Ships-of-the-Desert-Web-min.jpg",
 		},
 		{
 			name: "Made in England",
-			src: "/oil/17-Made-in-England-Web.jpg",
+			src: "/oil/17-Made-in-England-Web-min.jpg",
 		},
 		{
 			name: "Vital Heritage",
-			src: "/oil/18-Vital-Heritage-Web.jpg",
+			src: "/oil/18-Vital-Heritage-Web-min.jpg",
 		},
 		{
 			name: "My son Ton Fils Ihr Sohn Your Son",
-			src: "/oil/20-My-son-Ton-Fils-Ihr-Sohn-Your-Son-Web.jpg",
+			src: "/oil/20-My-son-Ton-Fils-Ihr-Sohn-Your-Son-Web-min.jpg",
 		},
 		{
 			name: "Them",
-			src: "/oil/22-Them-Web.jpg",
+			src: "/oil/22-Them-Web-min.jpg",
 		},
 		{
 			name: "Side by Side",
-			src: "/oil/24-Side-by-Side-Web.jpg",
+			src: "/oil/24-Side-by-Side-Web-min.jpg",
 		},
 		{
 			name: "The Drone",
-			src: "/oil/26The-Drone-Web.jpg",
+			src: "/oil/26The-Drone-Web-min.jpg",
 		},
 		{
 			name: "The Abomination of Desolation",
-			src: "/oil/27-The-Abomination-of-Desolation-Web.jpg",
+			src: "/oil/27-The-Abomination-of-Desolation-Web-min.jpg",
 		},
 		{
 			name: "The Vulture",
-			src: "/oil/28-The-Vulture-Web.jpg",
+			src: "/oil/28-The-Vulture-Web-min.jpg",
 		},
 	],
 	[ImageType.Prints]: [
 		{
 			name: "The Retired Tinman",
-			src: "/prints/1The-Retired-Tinman-Web.jpg",
+			src: "/prints/1The-Retired-Tinman-Web-min.jpg",
 		},
 		{
 			name: "Made in England",
-			src: "/prints/2-Made-in-England.JPG",
+			src: "/prints/2-Made-in-England-min.JPG",
 		},
 		{
 			name: "The Air Raid",
-			src: "/prints/2-The-Air-Raid-Web.jpg",
+			src: "/prints/2-The-Air-Raid-Web-min.jpg",
 		},
 		{
 			name: "Sector Six",
-			src: "/prints/3-Sector-Six.JPG",
+			src: "/prints/3-Sector-Six-min.JPG",
 		},
 		{
 			name: "In the light of Truth",
-			src: "/prints/4-In-the-light-of-Truth-Web-.jpg",
+			src: "/prints/4-In-the-light-of-Truth-Web--min.jpg",
 		},
 		{
 			name: "Passing on the Torch",
-			src: "/prints/6-Passing-on-the-Torch-Web.jpg",
+			src: "/prints/6-Passing-on-the-Torch-Web-min.jpg",
 		},
 		{
 			name: "Here Come the Glass Beads Folks",
-			src: "/prints/7-Here-Come-the-Glass-Beads-Folks.jpg",
+			src: "/prints/7-Here-Come-the-Glass-Beads-Folks-min.jpg",
 		},
 		{
 			name: "Hostility",
-			src: "/prints/9-Hostility-Web.jpg",
+			src: "/prints/9-Hostility-Web-min.jpg",
 		},
 		{
 			name: "Penge defies the Swastika",
-			src: "/prints/9a-Penge-defies-the-Swastika.JPG",
+			src: "/prints/9a-Penge-defies-the-Swastika-min.JPG",
 		},
 		{
 			name: "Docks",
-			src: "/prints/14-Docks-1-Webl.JPG",
+			src: "/prints/14-Docks-1-Webl-min.JPG",
 		},
 		{
 			name: "Opportunity Knocks",
-			src: "/prints/19-Opportunity-Knocks-Web-.jpg",
+			src: "/prints/19-Opportunity-Knocks-Web--min.jpg",
 		},
 		{
 			name: "We good them bad",
-			src: "/prints/21-We-good-them-bad-Web.jpg",
+			src: "/prints/21-We-good-them-bad-Web-min.jpg",
 		},
 		{
 			name: "Incarceration",
-			src: "/prints/23-Incarceration-Web.jpg",
+			src: "/prints/23-Incarceration-Web-min.jpg",
 		},
 		{
 			name: "Happy Town",
-			src: "/prints/25-Happy-Town-Web.jpg",
+			src: "/prints/25-Happy-Town-Web-min.jpg",
 		},
 		{
 			name: "Uncanny Church",
-			src: "/prints/t0-Uncanny-Church.JPG",
+			src: "/prints/t0-Uncanny-Church-min.JPG",
 		},
 		{
 			name: "Reggae Reggae Reggae",
-			src: "/prints/t2-Reggae-Reggae-Reggae.JPG",
+			src: "/prints/t2-Reggae-Reggae-Reggae-min.JPG",
 		},
 		{
 			name: "The Fall of Man",
-			src: "/prints/t6-The-Fall-of-Man.jpg",
+			src: "/prints/t6-The-Fall-of-Man-min.jpg",
 		},
 		{
 			name: "The Drone",
-			src: "/prints/t7-The-Drone.jpg",
+			src: "/prints/t7-The-Drone-min.jpg",
 		},
 		{
 			name: "Fathers Return",
-			src: "/prints/u0-Fathers-Return.JPG",
+			src: "/prints/u0-Fathers-Return-min.JPG",
 		},
 		{
 			name: "Breakout",
-			src: "/prints/u1-Breakout.JPG",
+			src: "/prints/u1-Breakout-min.JPG",
 		},
 		{
 			name: "Folkstone",
-			src: "/prints/u2Folkstone.jpg",
+			src: "/prints/u2Folkstone-min.jpg",
 		},
 		{
 			name: "Brave Old World",
-			src: "/prints/u4-Brave-Old-World.jpg",
+			src: "/prints/u4-Brave-Old-World-min.jpg",
 		},
 		{
 			name: "Golem gone West",
-			src: "/prints/u6-Golem-gone-West.jpg",
+			src: "/prints/u6-Golem-gone-West-min.jpg",
 		},
 		{
 			name: "Hanging on for Grim Life",
-			src: "/prints/u8-Hanging-on-for-Grim-Life.jpg",
+			src: "/prints/u8-Hanging-on-for-Grim-Life-min.jpg",
 		},
 		{
 			name: "I'm Superman I can Fly",
-			src: "/prints/u9-Im-Superman-I-can-Fly.jpg",
+			src: "/prints/u9-Im-Superman-I-can-Fly-min.jpg",
 		},
 		{
 			name: "Some New Ideas",
-			src: "/prints/v0-Some-New-Ideas.jpg",
+			src: "/prints/v0-Some-New-Ideas-min.jpg",
 		},
 		{
 			name: "Honey I'm Home",
-			src: "/prints/v1-Honey-Im-Home.jpg",
+			src: "/prints/v1-Honey-Im-Home-min.jpg",
 		},
 		{
 			name: "Fragments",
-			src: "/prints/v4-Fragments.jpg",
+			src: "/prints/v4-Fragments-min.jpg",
 		},
 		{
 			name: "Firm but Fair",
-			src: "/prints/v5-Firm-but-Fair.jpg",
+			src: "/prints/v5-Firm-but-Fair-min.jpg",
 		},
 		{
 			name: "June 2014",
-			src: "/prints/v6-June-2014.jpg",
+			src: "/prints/v6-June-2014-min.jpg",
 		},
 		{
 			name: "A Dance of Love",
-			src: "/prints/v7-A-Dance-of-Love.jpg",
+			src: "/prints/v7-A-Dance-of-Love-min.jpg",
 		},
 	],
 	[ImageType.Watercolours]: [
 		{
 			name: "Container Town",
-			src: "/watercolours/A3-Container-Town-2-dark-paler1.jpg",
+			src: "/watercolours/A3-Container-Town-2-dark-paler1-min.jpg",
 		},
 		{
 			name: "Barns",
-			src: "/watercolours/Barns.jpg",
+			src: "/watercolours/Barns-min.jpg",
 		},
 		{
 			name: "Blue still life with bottle",
-			src: "/watercolours/Blue-still-life-with-bottle.jpg",
+			src: "/watercolours/Blue-still-life-with-bottle-min.jpg",
 		},
 		{
 			name: "Bus Queue",
-			src: "/watercolours/Bus-Queue.jpg",
+			src: "/watercolours/Bus-Queue-min.jpg",
 		},
 		{
 			name: "Compound at Martlesham",
-			src: "/watercolours/Compound-at-Martlesham.jpg",
+			src: "/watercolours/Compound-at-Martlesham-min.jpg",
 		},
 		{
 			name: "Countryside near Fingeringhoe",
-			src: "/watercolours/Countryside-near-Fingeringhoe.jpg",
+			src: "/watercolours/Countryside-near-Fingeringhoe-min.jpg",
 		},
 		{
 			name: "Dominique and Luke on the shore at Biarritz",
-			src: "/watercolours/Dominique-and-Luke-on-the-shore-at-Biarritz-mod-for-IAS.jpg",
+			src: "/watercolours/Dominique-and-Luke-on-the-shore-at-Biarritz-mod-for-IAS-min.jpg",
 		},
 		{
 			name: "Fiona",
-			src: "/watercolours/Fiona-Copy.jpg",
+			src: "/watercolours/Fiona-Copy-min.jpg",
 		},
 		{
 			name: "Futurist House",
-			src: "/watercolours/Futurist-House.jpg",
+			src: "/watercolours/Futurist-House-min.jpg",
 		},
 		{
 			name: "Grapes wine and a flagon",
-			src: "/watercolours/Grapes-wine-and-a-flagon.jpg",
+			src: "/watercolours/Grapes-wine-and-a-flagon-min.jpg",
 		},
 		{
 			name: "Guinean Scene with Man in White",
-			src: "/watercolours/Guinean-Scene-with-Man-in-White-Large.jpg",
+			src: "/watercolours/Guinean-Scene-with-Man-in-White-Large-min.jpg",
 		},
 		{
 			name: "Hollyhock Garden",
-			src: "/watercolours/Hollyhock-Garden.jpg",
+			src: "/watercolours/Hollyhock-Garden-min.jpg",
 		},
 		{
 			name: "In the Grove at Felixstowe",
-			src: "/watercolours/In-the-Grove-at-Felixstowe.jpg",
+			src: "/watercolours/In-the-Grove-at-Felixstowe-min.jpg",
 		},
 		{
 			name: "Lilies",
-			src: "/watercolours/Lilies.jpg",
+			src: "/watercolours/Lilies-min.jpg",
 		},
 		{
 			name: "Lorry in the Pyrenees",
-			src: "/watercolours/Lorry-in-the-Pyrenees.jpg",
+			src: "/watercolours/Lorry-in-the-Pyrenees-min.jpg",
 		},
 		{
 			name: "Luke Maran",
-			src: "/watercolours/Luke-Maran-2.jpg",
+			src: "/watercolours/Luke-Maran-2-min.jpg",
 		},
 		{
 			name: "Mushrooms and Toadstools",
-			src: "/watercolours/Mushrooms-and-Toadstools-3.jpg",
+			src: "/watercolours/Mushrooms-and-Toadstools-3-min.jpg",
 		},
 		{
 			name: "Mushrooms and Toadstools 2",
-			src: "/watercolours/Mushrooms-and-Toadstools-new.jpg",
+			src: "/watercolours/Mushrooms-and-Toadstools-new-min.jpg",
 		},
 		{
 			name: "Old Vinyard in Provence",
-			src: "/watercolours/Old-Vinyard-in-Provence.jpg",
+			src: "/watercolours/Old-Vinyard-in-Provence-min.jpg",
 		},
 		{
 			name: "On the prom in the rain",
-			src: "/watercolours/On-the-prom-in-the-rain.jpg",
+			src: "/watercolours/On-the-prom-in-the-rain-min.jpg",
 		},
 		{
 			name: "Redoubt in Mediterranean",
-			src: "/watercolours/Redooubt-in-Mediterranean-2.jpg",
+			src: "/watercolours/Redooubt-in-Mediterranean-2-min.jpg",
 		},
 		{
 			name: "Spring with Luke and Kimberley",
-			src: "/watercolours/Spring-with-Luke-and-Kimberley.jpg",
+			src: "/watercolours/Spring-with-Luke-and-Kimberley-min.jpg",
 		},
 		{
 			name: "The French Lieutenants woman",
-			src: "/watercolours/The-French-Lieutentants-woman.jpg",
+			src: "/watercolours/The-French-Lieutentants-woman-min.jpg",
 		},
 		{
 			name: "You Beautiful Doll",
-			src: "/watercolours/You-Beautiful-Doll-hlf.jpg",
+			src: "/watercolours/You-Beautiful-Doll-hlf-min.jpg",
 		},
 	],
 };
