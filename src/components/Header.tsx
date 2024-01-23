d9a930e90144a1acbe80a7982fafd3e60ab53eb4
Edgar Nightingale <edgar.nightingale@geniussports.com>
2024-01-23 17:41:13 +0000

    Add mobile navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,21 @@
 "use client";
 
-import { caveat, josefin } from "@/app/fonts";
-import { navigation } from "@/utils/constants";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { josefin } from "@/app/fonts";
+import { DesktopNavigation, MobileNavigation } from "./Navigation";
 
 export default function Header() {
-	const pathname = usePathname();
-
 	return (
-		<header className="p-4 mb-8">
+		<header className="py-4 mb-8">
 			<div className="container mx-auto px-4">
+				<MobileNavigation />
 				<div className="header-image">
-					<h1 className={`${josefin.className} text-center text-6xl font-bold`}>Charles Nightingale</h1>
-					<h2 className={`${josefin.className} text-center font-thin text-4xl`}>Artist</h2>
+					<h1 className={`${josefin.className} text-center text-4xl md:text-6xl font-bold`}>
+						Charles Nightingale
+					</h1>
+					<h2 className={`${josefin.className} text-center text-2xl md:text-4xl font-thin`}>Artist</h2>
 				</div>
 
-				<div className={`${caveat.className} flex justify-center items-center gap-16`}>
-					{navigation.map((item) => {
-						const active = pathname === item.href ? "active" : "";
-
-						return (
-							<Link key={item.label} className="text-2xl" href={item.href}>
-								<span className={`${active} link link-underline`}>{item.label}</span>
-							</Link>
-						);
-					})}
-				</div>
+				<DesktopNavigation />
 			</div>
 		</header>
 	);
