be8dec920861d656197de6dfd5027d912f8fe27c
Edgar Nightingale <edgar.nightingale@geniussports.com>
2024-01-24 10:47:28 +0000

    Updae title font weight

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,9 +9,7 @@ export default function Header() {
 			<div className="container mx-auto px-4">
 				<MobileNavigation />
 				<div className="header-image">
-					<h1 className={`${josefin.className} text-center text-4xl md:text-6xl font-bold`}>
-						Charles Nightingale
-					</h1>
+					<h1 className={`${josefin.className} text-center text-4xl md:text-6xl`}>Charles Nightingale</h1>
 					<h2 className={`${josefin.className} text-center text-2xl md:text-4xl font-thin`}>Artist</h2>
 				</div>
 
